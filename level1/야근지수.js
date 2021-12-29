3b111097a5709bedee4fcbd5e88cd31ce91bac8e
imgwang-yeol <mkitigy@gmail.com>
2021-12-29 09:24:07 +0900

    level1 - 야근지수

---
+++ level1/야근지수.js
@@ -0,0 +1,22 @@
+function solution(n, works) {
+  const totalWork = works.reduce((total, work) => total + work);
+
+  if (n >= totalWork) return 0;
+
+  let maxWork = works.reduce((max, work) => Math.max(max, work));
+
+  while (n > 0) {
+    for (let i = 0; i < works.length; i++) {
+      if (works[i] !== maxWork) continue;
+
+      works[i]--;
+      n--;
+
+      if (n === 0) break;
+    }
+
+    maxWork--;
+  }
+
+  return works.reduce((total, work) => total + Math.pow(work, 2), 0);
+}
